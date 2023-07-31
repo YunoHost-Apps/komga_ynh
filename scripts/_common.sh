86d6510f1042bc5aaf051dda106170e768304a54
Navan Chauhan <navanchauhan@gmail.com>
2023-07-31 13:42:14 -0600

    bumping JRE to 17

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="openjdk-11-jre-headless"
+pkg_dependencies="openjdk-17-jre-headless"
 
 #=================================================
 # PERSONAL HELPERS
